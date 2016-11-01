7774feba2f65ccd8bcf40fb257c398de3807d086
Brian Smith <brian.smith.keith@gmail.com>
2016-11-01 11:22:16 -0400

    cleaned up debug

--- spec/features/gmail_test.rb
+++ spec/features/gmail_test.rb
@@ -1,38 +1,17 @@
 require 'spec_helper'
 
 feature 'I want to implement futures to test gmail validation' do
-  include Minitest::Capybara::Behaviour
-
-  let(:options) {{ debug: true }}
-  let(:global)  {{}}
-
-  # background do
-  #   global[:confirmation] = rand(100000...999999)
-  #   print "Confirmation ##{global[:confirmation]}"
-  # end
 
   scenario 'Login and get inbox count' do
-
     email_address = ENV['email']
     pass          = ENV['pass']
-    gmail = Gmail.connect!(email_address, pass)
-    raise "ERROR: Inbox was lower than expected" unless gmail.inbox.count > 1
+    gmail_inbox_count = Gmail.connect!(email_address, pass).inbox.count
+    raise "ERROR: Inbox was lower than expected" unless gmail_inbox_count > 1
     print "     Im done with the inbox\n".yellow
-    # puts "inbox count = #{gmail.inbox.count}"
-
-    # email = gmail.compose do
-    #   binding.pry
-    #   to email_address
-    #   subject "Confirmation ##{@conf} Having fun in Puerto Rico!"
-    #   body "Spent the day on the road...\nagain, confirmation ##{@conf}\n\nLove always, B-drizzle"
-    # end
-    # email.deliver!
-
   end
 
   scenario 'Second it block with no wait' do
     print "     No waiting for me...\n".cyan
-    true
   end
 
 end
